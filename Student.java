0502bbef23c6375e55a8e9fe4b438b593c3bb509
Dev gandhi <devgandhi2705@gmail.com>
2025-04-20 03:26:32 +0530

    declare variables and class student

--- Student.java
+++ Student.java
@@ -8,6 +8,17 @@ package com.studentdata;
  */
 
 public class Student {
+private String prn;
+    private String name;
+    private String dob;
+    private double marks;
+
+    public Student(String prn, String name, String dob, double marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
 
 
 
